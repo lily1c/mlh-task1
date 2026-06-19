78c99e6bb730897fde8113727451189b0df615c1
lily1c <125397761+lily1c@users.noreply.github.com>
2026-06-19 12:33:39 +0500

    Add dynamic sidebar menu bar driven by pages list

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+pages = [
+    {"name":"Home", "url":"/"},
+   # {"name":"Hobbies", "url":"/hobbies"},
+
+]
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), pages=pages)
+
