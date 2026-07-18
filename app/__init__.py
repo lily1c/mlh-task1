b5b81d294b5e3123b58db765ecdeabb32ee91a03
Assol Abasova <125397761+lily1c@users.noreply.github.com>
2026-07-18 22:08:53 +0500

    Refactor database connection logic
    Refactored database initialization to use SqliteDatabase in test mode and MySQLDatabase in production mode.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import MySQLDatabase
+from peewee import MySQLDatabase, SqliteDatabase, Model, CharField, TextField, DateTimeField
+
 from .writing import get_articles
 from playhouse.shortcuts import model_to_dict
 from peewee import MySQLDatabase, Model, CharField, TextField, DateTimeField
@@ -12,14 +14,19 @@ load_dotenv()
 app = Flask(__name__)
 
 
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306,
+    )
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306,
-)
+
 
 print(mydb)
 
